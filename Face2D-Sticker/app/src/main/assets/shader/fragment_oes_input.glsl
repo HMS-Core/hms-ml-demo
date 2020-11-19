5c4b8fc641809ad135ab41eac2dfec9c4306b815
niechaopeng <niechaopeng@huawei.com>
2020-11-19 19:18:27 +0800

    Update Demo

--- Face2D-Sticker/app/src/main/assets/shader/fragment_oes_input.glsl
+++
@@ -1,7 +0,0 @@
-#extension GL_OES_EGL_image_external : require
-precision mediump float;
-varying vec2 textureCoordinate;
-uniform samplerExternalOES inputTexture;
-void main() {
-    gl_FragColor = texture2D(inputTexture, textureCoordinate);
-}
